8d99f14898e28a1727e38e550450fcb7cdcd93dd
Ciaran O'Donnell <ciaran.odonnell@avanade.com>
2021-11-10 18:49:23 -0500

    2

--- ASBTesting.ts
+++ ASBTesting.ts
@@ -1,5 +1,13 @@
-import { ServiceBusClient } from "@azure/service-bus";
+import * as asb from "@azure/service-bus";
+// import { ServiceBusAdministrationClient } from "@azure/service-bus";
+// import { CreateSubscriptionOptions } from "@azure/service-bus";
 
+
+
+
+// Load the .env file if it exists
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const uuid = require('uuid')
 
@@ -11,20 +19,32 @@ class ASBTest{
 
     connectionString : string;
     correlationId : string;
+    admin : asb.ServiceBusAdministrationClient;
+    sbClient :  asb.ServiceBusClient;
 
     constructor(connectionString : string){
 
         this.connectionString = connectionString;
         this.correlationId = uuid();
 
-        this.serviceBusClient = new ServiceBusClient("Endpoint=sb://ciaransyoutubedemos.servicebus.windows.net/;SharedAccessKeyName=SendDemo;SharedAccessKey=LJ2juCgL6hebPIoXyF3q9EVp17MswMMIj2eVVQ6LCis=");
 
-        const sender = serviceBusClient.createSender("demoqueue");
+        // You can also use AAD credentials from `@azure/identity` along with the host url
+        // instead of the connection string for authentication.
+        this.admin = new  asb.ServiceBusAdministrationClient(connectionString);
+
+        const sbClient = new  asb.ServiceBusClient(connectionString);
+
+        
         
 
     }
 
-    subscribeToTopic(topic : string){
+    async subscribeToTopic(topic : string){
+        const options = new  asb.CreateSubscriptionOptions();
+        options.deadLetteringOnMessageExpiration = false;
+        
+        await this.admin.createSubscription(topic, "testsub-" + this.correlationId, options);
+
 
     }
 
---
+++ test.ts
@@ -0,0 +1,3 @@
+import ASBTest from "./ASBTesting";
+
+var test = new ASBTest("Endpoint=sb://ciaransyoutubedemos.servicebus.windows.net/;SharedAccessKeyName=SendDemo;SharedAccessKey=LJ2juCgL6hebPIoXyF3q9EVp17MswMMIj2eVVQ6LCis=");
